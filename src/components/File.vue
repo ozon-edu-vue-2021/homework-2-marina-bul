7bc223a195150bcb6b7738d182f3d9544517d9f5
Bulanova Marina <bragina_marina@mail.ru>
2021-10-25 15:21:30 +0300

    Homework done

---
+++ src/components/File.vue
@@ -0,0 +1,80 @@
+<template>
+  <div
+    @click="makeActive"
+    :class="{ active: isActive }"
+  >
+    <p
+      v-if="isFile"
+      class="file"
+    >
+      {{ item.name }}
+    </p>
+    <p
+      v-if="isLink"
+      class="link"
+    >
+      {{ item.name }}
+    </p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "File",
+  props: {
+    item: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+  computed: {
+    isFile() {
+      return this.item.type === "file";
+    },
+    isLink() {
+      return this.item.type === "link";
+    },
+  },
+  methods: {
+    makeActive() {
+      this.isActive = true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+p {
+  margin: 3px 0;
+  padding: 5px;
+  width: 20rem;
+  cursor: pointer;
+}
+
+.file {
+  color: rgb(232, 241, 97);
+}
+
+.active .file {
+  color: #fff;
+  background-color: rgba(197, 211, 9, 0.5);
+  border-radius: 7px;
+}
+
+.link {
+  color: #fff;
+  font-style: italic;
+  text-decoration: underline;
+}
+
+.active .link {
+  color: #233329;
+  background-color: #81da8c;
+  border-radius: 7px;
+}
+</style>
---
+++ src/components/Folder.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div
+      @click="toggle"
+      class="folder"
+    >
+      <span>{{ folderItem.name }}</span>
+      <span class="icon">[{{ isOpened ? "-" : "+" }}]</span>
+    </div>
+    <tree-item
+      :items="folderItem.contents"
+      v-if="isOpened"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Folder",
+  props: {
+    folderItem: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      isOpened: false,
+    };
+  },
+
+  methods: {
+    toggle() {
+      this.isOpened = !this.isOpened;
+    },
+  },
+
+  beforeCreate: function () {
+    this.$options.components.TreeItem = require("./TreeItem.vue").default;
+  },
+};
+</script>
+
+<style scoped>
+.folder {
+  margin: 7px 0;
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
+}
+.icon {
+  margin-left: 10px;
+}
+</style>
